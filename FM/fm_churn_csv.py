2a629089044a1f5a0cb22e28f44028316fb66a95
xiaogp <gengpeng@cloudwalk.cn>
2019-10-25 17:04:04 +0800

    first commit

--- FM/fm_churn_csv.py
+++ FM/fm_churn_csv.py
@@ -11,31 +11,18 @@ import tensorflow as tf
 from tensorflow.python.saved_model import tag_constants
 
 
-continous_cols = ["shop_duration", "recent", "monetary", "max_amount", 
-                  "save_amount", "items_count", "valid_points_sum", 
-                  "member_day", "frequence", "avg_amount", "item_count_turn", 
-                  "avg_piece_amount", "shops_count", "promote_percent", 
-                  "wxapp_diff", "store_diff", "week_percent"]
-
-category_cols = ["CHANNEL_NUM_ID", "VIP_TYPE_NUM_ID", "shop_channel", 
-                 "infant_group", "water_product_group", "meat_group", 
-                 "beauty_group", "health_group", "fruits_group", 
-                 "vegetables_group", "pets_group", "snacks_group", 
-                 "smoke_group", "milk_group", "instant_group", "grain_group"]
-
-
 class FM(object):
     def __init__(self, feature_size, fm_v_size=8, loss_fuc="Cross_entropy", train_optimizer="Adam", 
-                 learning_rate=0.1, reg_type="l2_reg", reg_param=0.001, decaylearning_rate=0.9):
+                 learning_rate=0.1, reg_type="l2_reg", reg_param_w=0.0, reg_param_v=0.0, decaylearning_rate=0.9):
         self.input_x = tf.placeholder(tf.float32, [None, feature_size], name="input_x")
         self.input_y = tf.placeholder(tf.float32, [None, 1], name="input_y")
         self.global_step = tf.Variable(0, name="global_step", trainable=False)
         
         with tf.name_scope("fm_layers"):
             # 一阶系数
-            FM_W = tf.Variable(tf.random_normal(shape=[feature_size]), name="fm_beta1")
+            FM_W = tf.get_variable(shape=[feature_size], initializer=tf.glorot_normal_initializer(), name="fm_beta1")
             # 二阶交互项,n×k
-            FM_V = tf.Variable(tf.random_normal(shape=[feature_size, fm_v_size]), name="fm_beta2")
+            FM_V = tf.get_variable(shape=[feature_size, fm_v_size], initializer=tf.glorot_normal_initializer, name="fm_beta2")
             # 常数项
             FM_B = tf.Variable(tf.constant(0.0), dtype=tf.float32, name="fm_bias")  # W0
             
@@ -63,11 +50,12 @@ class FM(object):
             
         with tf.name_scope("loss"):
             if reg_type == 'l1_reg':
-                regularization = tf.contrib.layers.l1_regularizer(reg_param)(FM_W)
+                regularization = tf.contrib.layers.l1_regularizer(reg_param_w)(FM_W) + \
+                                 tf.contrib.layers.l1_regularizer(reg_param_v)(FM_V)
             elif reg_type == 'l2_reg':
-                regularization = reg_param * tf.nn.l2_loss(FM_W)
+                regularization = reg_param_w * tf.nn.l2_loss(FM_W) + reg_param_v * tf.nn.l2_loss(FM_V)
             else:
-                regularization = reg_param * tf.nn.l2_loss(FM_W)
+                regularization = reg_param_w * tf.nn.l2_loss(FM_W) + reg_param_v * tf.nn.l2_loss(FM_V)
             if loss_fuc == 'Squared_error':
                 self.loss = tf.reduce_mean(tf.reduce_sum(tf.square(self.input_y - self.output), 
                                                     reduction_indices=[1])) + regularization
@@ -77,7 +65,7 @@ class FM(object):
                         labels=tf.reshape(self.input_y, [-1]))) + regularization
             
         with tf.name_scope("optimizer"):
-            if decaylearning_rate:
+            if decaylearning_rate != 1:
                 learning_rate = tf.train.exponential_decay(learning_rate, self.global_step, 100, decaylearning_rate)
             if train_optimizer == 'Adam':
                 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
@@ -98,26 +86,20 @@ class FM(object):
             
 def preprocessing():
     """对数据进行onehot和minmax处理，分割训练和测试样本，pickle序列化"""
-    df = pd.read_csv("./churn.csv", sep=",")
+    df = pd.read_csv("./churn2.csv", sep=",")
     train, test = train_test_split(df, test_size=0.2)
     
     onehot_transform = OneHotEncoder(categories="auto", handle_unknown="ignore")
-    minmax_transform = MinMaxScaler()
-    
-    train_category_sparse = onehot_transform.fit_transform(train[category_cols]).toarray()
-    test_category_sparse = onehot_transform.transform(test[category_cols]).toarray()
-    
-    train_feature = minmax_transform.fit_transform(np.hstack(
-            (np.array(train[continous_cols]), train_category_sparse)))
-    test_feature = minmax_transform.transform(np.hstack(
-            (np.array(test[continous_cols]), test_category_sparse)))
+    category_cols = train.columns.difference(["label", "USR_NUM_ID"])
+    train_feature= onehot_transform.fit_transform(train[category_cols]).toarray()
+    test_feature = onehot_transform.transform(test[category_cols]).toarray()
     
     print("{0:-^60}".format("data overview"))
     print("训练样本数", train_feature.shape[0])
     print("训练样本数", test_feature.shape[0])
     print("特征数", train_feature.shape[1])
     
-    pickle.dump((onehot_transform, minmax_transform), open("./encoder_churn.pkl", "wb"))
+    pickle.dump(onehot_transform, open("./encoder_churn.pkl", "wb"))
     pickle.dump((train_feature, train["label"].values), open("./train_churn.pkl", "wb"))
     pickle.dump((test_feature, test["label"].values), open("./test_churn.pkl", "wb"))
 
@@ -134,15 +116,32 @@ def get_batch(epoches, batch_size):
 
 
 def train_main():
+    FLAGS = tf.app.flags.FLAGS
+    # model参数
+    tf.app.flags.DEFINE_integer("feature_size", 149, "number of fields")
+    tf.app.flags.DEFINE_integer("fm_v_size", 8, "number of implicit vector dimensions")
+    tf.app.flags.DEFINE_string("loss_fuc", "Cross_entropy", "loss function")
+    tf.app.flags.DEFINE_string("train_optimizer", "Adam", "optimizer method")
+    tf.app.flags.DEFINE_float("learning_rate", 0.1, "initial learning rate")
+    tf.app.flags.DEFINE_string("reg_type", "l2_reg", "regularization method")
+    tf.app.flags.DEFINE_float("reg_param_w", 0.0, "first order beta regularization param")
+    tf.app.flags.DEFINE_float("reg_param_v", 0.0, "second order beta regularization param")
+    tf.app.flags.DEFINE_float("decaylearning_rate", 0.9, "decay learning rate param")
+    # data参数
+    tf.app.flags.DEFINE_integer("epoches", 100, "number of data repeat time")
+    tf.app.flags.DEFINE_integer("batch_size", 2048, "number of train data each batch")
+    print(FLAGS)
     tf.reset_default_graph()
-    model = FM(feature_size=66, fm_v_size=10, reg_param=0.0, learning_rate=0.2)
-    
+    model = FM(FLAGS.feature_size, FLAGS.fm_v_size, FLAGS.loss_fuc, FLAGS.train_optimizer, 
+               FLAGS.learning_rate, FLAGS.reg_type, FLAGS.reg_param_w, FLAGS.reg_param_v, 
+               FLAGS.decaylearning_rate)
+
     with tf.Session() as sess:
         init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer(), tf.tables_initializer())
         sess.run(init_op)
         shutil.rmtree("./FM_churn.log", ignore_errors=True)
         writer = tf.summary.FileWriter("./FM_churn.log", sess.graph)
-        batches = get_batch(10, 512)
+        batches = get_batch(FLAGS.epoches, FLAGS.batch_size)
         x_test, y_test = pickle.load(open("./test_churn.pkl", "rb"))
         
         for batch in batches:
@@ -164,10 +163,11 @@ def train_main():
         feed_dict = {model.input_x: x_test, model.input_y: np.reshape(y_test, [-1, 1])}
         _, logit_val = sess.run([model.train_step,model.logit], feed_dict=feed_dict)
         
-        print(accuracy_score(y_test, [round(x) for x in logit_val]))
-        print(precision_score(y_test, [round(x) for x in logit_val]))
-        print(recall_score(y_test, [round(x) for x in logit_val]))
-        print(roc_auc_score(y_test, logit_val))
+        print("accuracy:", accuracy_score(y_test, [round(x) for x in logit_val]))
+        print("precision:", precision_score(y_test, [round(x) for x in logit_val]))
+        print("reall:", recall_score(y_test, [round(x) for x in logit_val]))
+        print("f1:", f1_score(y_test, [round(x) for x in logit_val]))
+        print("auc:", roc_auc_score(y_test, logit_val))
         
         # 模型保存
         shutil.rmtree("FM_churn.pb", ignore_errors=True)
@@ -183,12 +183,10 @@ def train_main():
         builder.save()
         
         
-def predict_mian():
-    test = pd.read_csv("./churn.csv").dropna()
-    onehot_transform, minmax_transform = pickle.load(open("./encoder_churn.pkl", "rb"))
-    test_category_sparse = onehot_transform.transform(test[category_cols]).toarray()
-    test_feature = minmax_transform.transform(np.hstack(
-            (np.array(test[continous_cols]), test_category_sparse)))
+def predict_main():
+    test = pd.read_csv("./churn2.csv").dropna()
+    onehot_transform = pickle.load(open("./encoder_churn.pkl", "rb"))
+    test_feature = onehot_transform.transform(test[test.columns.difference(["label", "USR_NUM_ID"])]).toarray()
     
     tf.reset_default_graph()
     with tf.Session() as sess:
@@ -206,4 +204,4 @@ def predict_mian():
 if __name__ == "__main__":
     preprocessing()
     train_main()
-    predict_mian()
+    predict_main()
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # customer_churn_prediction
-客户流失预测模型，基于用户的购物行为和用户画像，使用sparkML RF，tensorflow FM，xgboost GBDT建模
+使用sparkML RF，tensorflow FM 预测客户流失
